ca43c94fd1f57a6d35747bbc698b07101ed4dbad
pitzzahh <pitzzahh@gmail.com>
2022-11-19 20:24:00 +0800

    added some required imports

--- src/main/java/io/github/pitzzahh/jokes/controller/JokesController.java
+++ src/main/java/io/github/pitzzahh/jokes/controller/JokesController.java
@@ -1,13 +1,13 @@
 package io.github.pitzzahh.jokes.controller;
 
-import io.github.pitzzahh.jokes.entity.Joke;
-import io.github.pitzzahh.jokes.service.JokesService;
 import io.github.pitzzahh.util.utilities.classes.enums.Status;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import io.github.pitzzahh.jokes.service.JokesService;
+import io.github.pitzzahh.jokes.entity.Joke;
 
 @RestController
 @RequestMapping("api/v1/jokes")
@@ -27,6 +27,5 @@ public record JokesController(JokesService jokesService) {
     public Status saveAll() {
         return jokesService.saveAll();
     }
-
 
 }
