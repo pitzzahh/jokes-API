a0977575c0ab284ba99f6c9da8ef798a63e9a0bf
pitzzahh <araopj2191@gmail.com>
2022-11-22 08:43:42 +0800

    add custom exception

--- src/main/java/io/github/pitzzahh/jokes/controller/JokesController.java
+++ src/main/java/io/github/pitzzahh/jokes/controller/JokesController.java
@@ -25,12 +25,9 @@
 package io.github.pitzzahh.jokes.controller;
 
 import io.github.pitzzahh.util.utilities.classes.enums.Status;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PostMapping;
 import io.github.pitzzahh.jokes.service.JokesService;
+import org.springframework.web.bind.annotation.*;
 import org.json.simple.parser.ParseException;
 import io.github.pitzzahh.jokes.entity.Joke;
 import java.io.IOException;
@@ -42,6 +39,20 @@ public record JokesController(JokesService jokesService) {
     @RequestMapping("/random")
     public Joke getRandomJoke() {
         return jokesService.generateRandomJoke();
+    }
+
+    /**
+     * Gets a joke by its category
+     * @param category the category of the joke
+     * @return the joke
+     */
+    @RequestMapping(
+            value = "/random/{category}",
+            method = RequestMethod.GET,
+            produces = "application/json"
+    )
+    public Joke getJokeByCategory(@PathVariable String category) {
+        return jokesService.getJokeByCategory(category);
     }
 
     @PostMapping("/add")
