37a3f642d8dca4ed639da28f88c9f8be211a89b0
pitzzahh <pitzzahh@araopj.tech>
2023-02-03 14:34:29 +0800

    refactor: joke request controller

--- src/main/java/tech/araopj/jokes/requests/controller/JokeRequestController.java
+++ src/main/java/tech/araopj/jokes/requests/controller/JokeRequestController.java
@@ -7,25 +7,25 @@ import tech.araopj.jokes.requests.service.JokeRequestService;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import tech.araopj.jokes.requests.entity.JokeRequest;
+import tech.araopj.jokes.requests.entity.JokeRequestBody;
 import tech.araopj.jokes.service.JokesService;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import static java.lang.String.format;
 
 @RestController
-@RequestMapping("v1/requests")
+@RequestMapping("v1/request")
 public record JokeRequestController(
         JokesService jokesService,
         JokeRequestService jokeRequestService
 ) {
 
     @PostMapping("/add")
-    public HttpEntity<String> addJoke(@Validated @RequestBody JokeRequest jokeRequest) throws ResponseStatusException {
+    public HttpEntity<String> addJoke(@Validated @RequestBody JokeRequestBody jokeRequestBody) throws ResponseStatusException {
         jokeRequestService
                 .getAllJokeRequests()
                 .stream()
-                .filter(j -> j.getJoke().contains(jokeRequest.getJoke()) && j.getCategory().equals(jokeRequest.getCategory()))
+                .filter(j -> j.getJoke().contains(jokeRequestBody.joke()) && j.getCategory().equals(jokeRequestBody.category()))
                 .findAny()
                 .ifPresent(j -> {
                     throw new ResponseStatusException(HttpStatus.BAD_REQUEST, format("Joke %s already exists", j.getJoke()));
@@ -33,16 +33,12 @@ public record JokeRequestController(
 
         jokesService.getAllJokes()
                 .stream()
-                .filter(j -> j.getJoke().contains(jokeRequest.getJoke()) && j.getCategory().equals(jokeRequest.getCategory()))
+                .filter(j -> j.getJoke().contains(jokeRequestBody.joke()) && j.getCategory().equals(jokeRequestBody.category()))
                 .findAny()
                 .ifPresent(j -> {
                     throw new ResponseStatusException(HttpStatus.BAD_REQUEST, format("Joke %s already exists", j.getJoke()));
                 });
-        return jokeRequestService.submitJoke(jokeRequest);
+        return jokeRequestService.submitJoke(jokeRequestBody);
     }
 
-    @PostMapping("/delete")
-    public HttpEntity<String> deleteJoke(@Validated @RequestBody JokeRequest jokeRequest) throws ResponseStatusException {
-        return jokeRequestService.deleteJoke(jokeRequest);
-    }
 }
