16ae64e1d1c2437281f785f0ec47b14ecbc4ad29
pitzzahh <pitzzahh@araopj.tech>
2023-02-03 14:35:07 +0800

    refactor: service making joke request

--- src/main/java/tech/araopj/jokes/requests/service/JokeRequestService.java
+++ src/main/java/tech/araopj/jokes/requests/service/JokeRequestService.java
@@ -1,6 +1,7 @@
 package tech.araopj.jokes.requests.service;
 
 import tech.araopj.jokes.requests.repository.JokeRequestRepository;
+import tech.araopj.jokes.requests.entity.JokeRequestBody;
 import tech.araopj.jokes.requests.entity.JokeRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.http.HttpEntity;
@@ -13,14 +14,14 @@ public record JokeRequestService(JokeRequestRepository repository) {
         return repository.findAll();
     }
 
-    public HttpEntity<String> submitJoke(JokeRequest jokeRequest) {
-        repository.save(jokeRequest);
+    public HttpEntity<String> submitJoke(JokeRequestBody jokeRequestBody) {
+        repository.save(JokeRequest.builder()
+                .joke(jokeRequestBody.joke())
+                .category(jokeRequestBody.category())
+                .lang(jokeRequestBody.language())
+                .build()
+        );
         return new HttpEntity<>("Joke submitted successfully");
     }
 
-    public HttpEntity<String> deleteJoke(JokeRequest jokeRequest) {
-        return new HttpEntity<>(
-                repository.deleteJokeRequestById(jokeRequest.getId()) == 1 ? "Joke deleted successfully" : "Joke not found"
-        );
-    }
 }
