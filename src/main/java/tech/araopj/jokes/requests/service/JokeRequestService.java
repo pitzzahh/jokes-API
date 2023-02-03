2ce101e0c485a9d257402f92f4c397614e33678e
pitzzahh <pitzzahh@araopj.tech>
2023-02-03 14:45:43 +0800

    add: service method in checking jokes existence

--- src/main/java/tech/araopj/jokes/requests/service/JokeRequestService.java
+++ src/main/java/tech/araopj/jokes/requests/service/JokeRequestService.java
@@ -1,17 +1,23 @@
 package tech.araopj.jokes.requests.service;
 
+import tech.araopj.jokes.entity.Joke;
 import tech.araopj.jokes.requests.repository.JokeRequestRepository;
 import tech.araopj.jokes.requests.entity.JokeRequestBody;
 import tech.araopj.jokes.requests.entity.JokeRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.http.HttpEntity;
 import java.util.Collection;
+import java.util.Optional;
 
 @Service
 public record JokeRequestService(JokeRequestRepository repository) {
 
     public Collection<JokeRequest> getAllJokeRequests() {
         return repository.findAll();
+    }
+
+    public Optional<JokeRequest> doesJokeAlreadySubmitted(String joke) {
+        return repository.findJokeRequestByJoke(joke);
     }
 
     public HttpEntity<String> submitJoke(JokeRequestBody jokeRequestBody) {
