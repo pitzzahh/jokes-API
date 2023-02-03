f601f53c8b647596ec21c94557475134a11ca896
pitzzahh <pitzzahh@araopj.tech>
2023-02-03 14:43:57 +0800

    refactor: checking joke request before adding to db

--- src/main/java/tech/araopj/jokes/requests/controller/JokeRequestController.java
+++ src/main/java/tech/araopj/jokes/requests/controller/JokeRequestController.java
@@ -8,36 +8,22 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import tech.araopj.jokes.requests.entity.JokeRequestBody;
-import tech.araopj.jokes.service.JokesService;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import static java.lang.String.format;
 
 @RestController
 @RequestMapping("v1/request")
-public record JokeRequestController(
-        JokesService jokesService,
-        JokeRequestService jokeRequestService
-) {
+public record JokeRequestController(JokeRequestService jokeRequestService) {
 
     @PostMapping("/add")
     public HttpEntity<String> addJoke(@Validated @RequestBody JokeRequestBody jokeRequestBody) throws ResponseStatusException {
         jokeRequestService
-                .getAllJokeRequests()
-                .stream()
-                .filter(j -> j.getJoke().contains(jokeRequestBody.joke()) && j.getCategory().equals(jokeRequestBody.category()))
-                .findAny()
+                .doesJokeAlreadySubmitted(jokeRequestBody.joke())
                 .ifPresent(j -> {
-                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, format("Joke %s already exists", j.getJoke()));
+                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, format("Joke %s already exists and submitted", j.getJoke()));
                 });
 
-        jokesService.getAllJokes()
-                .stream()
-                .filter(j -> j.getJoke().contains(jokeRequestBody.joke()) && j.getCategory().equals(jokeRequestBody.category()))
-                .findAny()
-                .ifPresent(j -> {
-                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, format("Joke %s already exists", j.getJoke()));
-                });
         return jokeRequestService.submitJoke(jokeRequestBody);
     }
 
