842fbd62ae1d9f020da61e06aa5873d26302b5f8
pitzzahh <pitzzahh@araopj.tech>
2023-02-03 14:34:43 +0800

    refactor: joke requests repository

--- src/main/java/tech/araopj/jokes/requests/repository/JokeRequestRepository.java
+++ src/main/java/tech/araopj/jokes/requests/repository/JokeRequestRepository.java
@@ -3,20 +3,22 @@ package tech.araopj.jokes.requests.repository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import tech.araopj.jokes.requests.entity.JokeRequestBody;
 import org.springframework.data.jpa.repository.Query;
 import tech.araopj.jokes.requests.entity.JokeRequest;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface JokeRequestRepository extends JpaRepository<JokeRequest, Long> {
+public interface JokeRequestRepository extends JpaRepository<JokeRequest, Integer> {
 
 
-    @Query("SELECT j FROM joke_request j WHERE j.joke LIKE ?1")
-    Optional<JokeRequest> findJokeByJoke(String joke);
+    @Query("SELECT j FROM joke_requests j WHERE j.joke LIKE ?1")
+    Optional<JokeRequestBody> findJokeRequestByJoke(String joke);
 
     @Transactional
     @Modifying
-    @Query("DELETE FROM joke_request j WHERE j.id = ?1")
+    @Query("DELETE FROM joke_requests j WHERE j.id = ?1")
     int deleteJokeRequestById(long id);
+
 }
