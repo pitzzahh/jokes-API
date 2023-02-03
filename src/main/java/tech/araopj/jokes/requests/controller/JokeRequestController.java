f9d70947836d370fd7e59d73003cf82c92e9628e
pitzzahh <pitzzahh@araopj.tech>
2023-02-03 14:46:16 +0800

    add: mapping on getting all joke requests

--- src/main/java/tech/araopj/jokes/requests/controller/JokeRequestController.java
+++ src/main/java/tech/araopj/jokes/requests/controller/JokeRequestController.java
@@ -1,20 +1,24 @@
 package tech.araopj.jokes.requests.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import tech.araopj.jokes.requests.service.JokeRequestService;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import tech.araopj.jokes.requests.entity.JokeRequestBody;
+import tech.araopj.jokes.requests.entity.JokeRequest;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import static java.lang.String.format;
+import java.util.Collection;
 
 @RestController
 @RequestMapping("v1/request")
 public record JokeRequestController(JokeRequestService jokeRequestService) {
+
+    @GetMapping("/all")
+    public Collection<JokeRequest> getAllJokeRequests() {
+        return jokeRequestService.getAllJokeRequests();
+    }
 
     @PostMapping("/add")
     public HttpEntity<String> addJoke(@Validated @RequestBody JokeRequestBody jokeRequestBody) throws ResponseStatusException {
