6fd31db315eaa5e10be551b113b3908a61b09d0c
pitzzahh <pitzzahh@gmail.com>
2023-02-04 10:12:43 +0800

    add: query for approving/getting joke request

--- src/main/java/tech/araopj/jokes/request/repository/JokeRequestRepository.java
+++ src/main/java/tech/araopj/jokes/request/repository/JokeRequestRepository.java
@@ -6,18 +6,27 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import tech.araopj.jokes.request.entity.JokeRequest;
 import org.springframework.stereotype.Repository;
+import java.util.Collection;
 import java.util.Optional;
 
 @Repository
 public interface JokeRequestRepository extends JpaRepository<JokeRequest, Integer> {
 
-
     @Query("SELECT j FROM joke_requests j WHERE j.joke LIKE ?1 OR j.joke = ?1")
     Optional<JokeRequest> findJokeRequestByJoke(String joke);
 
-    @Transactional
+    @Query("SELECT j FROM joke_requests j WHERE j.approved = false")
+    Collection<JokeRequest> getJokeRequestByApprovedNot();
+
+    @Query("SELECT j FROM joke_requests j WHERE j.approved = true")
+    Collection<JokeRequest> getJokeRequestByApproved();
+
     @Modifying
+    @Transactional
     @Query("DELETE FROM joke_requests j WHERE j.id = ?1")
     int deleteJokeRequestById(long id);
 
+    @Modifying
+    @Query("UPDATE joke_requests j SET j.approved = true WHERE j.id = ?1")
+    void approveRequest(JokeRequest jokeRequest);
 }
