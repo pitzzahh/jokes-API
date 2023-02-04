eec99b043995b0d152c9684df6a8bb2698c266b8
pitzzahh <pitzzahh@gmail.com>
2023-02-04 10:08:50 +0800

    add: endpoint for approving joke requests

--- src/main/java/tech/araopj/jokes/request/controller/JokeRequestController.java
+++ src/main/java/tech/araopj/jokes/request/controller/JokeRequestController.java
@@ -24,11 +24,19 @@ public record JokeRequestController(JokeRequestService jokeRequestService) {
     public HttpEntity<String> addJoke(@Validated @RequestBody JokeRequestBody jokeRequestBody) throws ResponseStatusException {
         jokeRequestService
                 .doesJokeAlreadySubmitted(jokeRequestBody.joke())
-                .ifPresent(j -> {
-                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, format("Joke %s already exists and submitted", j.getJoke()));
+                .ifPresent(joke -> {
+                    throw new ResponseStatusException(
+                            HttpStatus.BAD_REQUEST,
+                            format("Joke %s already exists and submitted", joke.getJoke())
+                    );
                 });
-
         return jokeRequestService.submitJoke(jokeRequestBody);
+    }
+
+    @PostMapping("/approve")
+    public HttpEntity<String> approveJoke(@RequestParam(name = "joke_id") int id) {
+        jokeRequestService.approveJoke(id);
+        return new HttpEntity<>("Joke approved successfully");
     }
 
 }
